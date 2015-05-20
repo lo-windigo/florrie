b6dda4f581f7c33af6c8e093023285465bee4bec
Windigo <jacob@fragdev.com>
2015-05-19 19:46:47 -0700

    Beginning test cases for core functionality, controller

---
+++ florrie/tests/core.php
@@ -0,0 +1,34 @@
+<?php
+/*
+	Florrie core functionality tests
+	Copyright © 2015 Jacob Hume
+
+	This file is part of Florrie.
+
+	Florrie is free software: you can redistribute it and/or modify it
+	under the terms of the GNU Affero General Public License as published
+	by the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Florrie is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with Florrie.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/florrie/lib/simpletest/autorun.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/florrie/florrie.php';
+
+
+class TestCore extends UnitTestCase {
+
+	function testBasics() {
+
+		$this->assertTrue(class_exists('Florrie'));
+	}
+}
+?>
